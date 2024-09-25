861f3b39e7cfa1651789da792e885bcf051fa8fa
omkar-2483 <kastureomkar03@gmail.com>
2024-09-25 11:37:26 +0530

    .

--- Client/src/components/posts/Posts.jsx
+++ Client/src/components/posts/Posts.jsx
@@ -4,17 +4,19 @@ import { useQuery } from "react-query";
 import { makeRequest } from "../../../Axios";
 
 function Posts() {
-  const { isLoading, error, posts } = useQuery("posts", () => 
-    makeRequest.get('/posts').then((res)=>{
+  const { isLoading, error, data } = useQuery("posts", () =>
+    makeRequest.get("/posts").then((res) => {
       return res.data;
     })
   );
 
   return (
     <div className="posts">
-      {posts.map((post) => (
-        <Post post={post} key={post.id} />
-      ))}
+      {error
+        ? "Something went wrong"
+        : isLoading
+        ? " Loading...."
+        : data.map((post) => <Post post={post} key={post.id} />)}
     </div>
   );
 }
